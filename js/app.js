3976560dab923978c0156a36e2bf440124220c2c
souad5017 <souadelbarjiji@gmail.com>
2026-09-17 14:29:50 +0100

    feat: add offer detail rendering

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 import { getData } from "./data.js"
-import { renderOffers } from "./render.js"
+import { renderOffers , renderOfferDetail } from "./render.js"
 
-async function init() {
-    try{
-        const offres = await getData()
-        renderOffers(offres)
-        // renderOfferDetail(offres) 
-        // console.log(offres)
+// async function init() {
+//     try {
+//         const offres = await getData()
+//         renderOffers(offres)
+//     }
+//     catch (error) {
+//         console.error(error)
+//     }
+// }
+
+async function initDetail() {
+    try {
+        const url = new URLSearchParams(window.location.search)
+        console.log(url)
+        renderOfferDetail(url);
     }
-    catch (error){
+    catch (error) {
         console.error(error)
     }
 }
-init()
+// const detailContainer = document.querySelector('#offer-detail')
+// if (!detailContainer) {
+
+    initDetail()
+// } else {
+  
+// }
+//   init()
